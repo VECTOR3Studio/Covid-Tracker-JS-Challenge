83234392195dc9b77de6f848020fcf80f7f37e78
VECTOR3Studio <47158115+VECTOR3Studio@users.noreply.github.com>
2022-01-05 17:33:16 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,29 @@
+// If you want to change the country you can overwrite the URL https://covid-api.mmediagroup.fr/v1/cases?country=<Country>
+
+const cases = document.getElementById('cases');
+// fetch API of covid19 in Slovakia
+fetch('https://covid-api.mmediagroup.fr/v1/cases?country=Slovakia')
+        .then(response => response.json())
+        .then(data => {
+            console.log(data)
+            cases.innerHTML = data.All.confirmed.toLocaleString(); 
+            document.getElementById('deaths').innerHTML = data.All.deaths.toLocaleString();
+            document.getElementById('population').innerHTML = data.All.population.toLocaleString();
+
+        // catch errors
+        }).catch(error => {
+            console.log(error)
+            
+        })
+    
+//fetch API of vaccine in Slovakia
+fetch('https://covid-api.mmediagroup.fr/v1//vaccines?country=Slovakia')
+        .then(response => response.json())
+        .then(data => {
+            console.log(data)
+            document.getElementById('vaccines').innerHTML = data.All.people_vaccinated.toLocaleString(); 
+            document.getElementById('partially').innerHTML = data.All.people_partially_vaccinated.toLocaleString(); 
+        }) .catch(error => {
+            console.log(error)
+            
+        })
